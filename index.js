e5278c2fd9095e1b106b309335fa6fc76c9620bb
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2022-11-25 09:52:03 -0800

    added code

--- index.js
+++ index.js
@@ -1,13 +1,165 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub Username?",
+        name: "githubname",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your username.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your email.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectname",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please the title of your project.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description",
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log('Please explain your project.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+
+    {
+        type: "input",
+        message: "What command should be run to install dependecencies?",
+        name: "dependencies",
+        validate: dependencyInput => {
+            if (dependencyInput) {
+                return true;
+            } else {
+                console.log('Please state the command to be used to run the dependencies.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please state the command to be used to run tests.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "repo",
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please tell your user about the repo.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing",
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please tell your user about the repo.');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise ((resolve, reject) => {
+        fs.writeFile('./generated-README.md', fileContent, err => {
+            if (err) {
+                reject (err) ;
+                return
+            }
+            resolve ({yes: true,
+            message: "Your file is created!"
+        });
+        });
+    });
+ };
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions).then((answers) => {
+        markdown(answers)
+    })
+};
 
 // Function call to initialize app
-init();
+init()
+.then(answers => {
+    console.log(answers);
+    return generateMarkdown(answers);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then (writeFileResponse => {
+    console.log (writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,106 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'nolicense') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+  } else {
+    return ' ';
+  }
+}
+
+// Function that returns license in table of contents
+// If there is no license, return an empty string
+function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+    return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+  
+  * [License](#license)
+  ${renderLicenseTOC(data.license)}
+  
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+\`\`\`
+  d
+\`\`\`
+  
+  ## Usage
+  
+  
+  
+  ## License
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  
+  
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+\`\`\`
+  npm test
+\`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly here 
+  [Email: ${data.email}](mailto:${data.email}). You can find more of my work at [cmarielorber](https://github.com/cmarielorber/).
 `;
 }
 
